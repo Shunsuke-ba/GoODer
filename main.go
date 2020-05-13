1e935d65dcca27c49135db73b6746aeee6b028f1
shunsuke-ba <monkey_shun.baske@icloud.com>
2020-05-13 15:17:12 +0900

    バッチ時間変更

--- main.go
+++ main.go
@@ -10,12 +10,11 @@ import (
 )
 
 func main() {
+	// バッチ処理用
+	scheduler.Every(3).Hours().Run(cron.CreateFavoriteCron) // 3時間ごと
+
 	// 手動で動かす用
 	http.HandleFunc("/", handler.CreateFavoriteHandler())
-
-	// バッチ処理用
-	scheduler.Every(3).Minutes().Run(cron.CreateFavoriteCron) // 2時間ごと
-
 	log.Println("Server Running")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("ListenAndServe is failed %v:", err)
